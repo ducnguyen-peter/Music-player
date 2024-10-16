de03786f92f126077ca07f13a63979914788f5ea
duc.nguyen27 <duc.nguyen27@onemount.com>
2024-10-16 17:04:54 +0700

    add permission request when app starts

---
+++ app/src/main/java/com/nguyenminhduc/musicplayer/data/pojo/MusicFile.kt
@@ -0,0 +1,9 @@
+package com.nguyenminhduc.musicplayer.data.pojo
+
+data class MusicFile(
+    val path: String? = null,
+    val title: String? = null,
+    val artist: String? = null,
+    val album: String? = null,
+    val duration: String? = null
+)
--- app/src/main/java/com/nguyenminhduc/musicplayer/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/nguyenminhduc/musicplayer/presentation/ui/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.nguyenminhduc.musicplayer.presentation.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.tabs.TabLayout
@@ -11,6 +17,10 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.nguyenminhduc.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_CODE_PERMISSION = 1
+    }
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = requireNotNull(_binding)
@@ -28,7 +38,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        requestPermission()
+    }
+
+    override fun onStart() {
+        super.onStart()
         setupUI()
+    }
+
+    private fun requestPermission() {
+        val permissionState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.READ_MEDIA_AUDIO)
+        } else {
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (permissionState != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_MEDIA_AUDIO), REQUEST_CODE_PERMISSION)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE_PERMISSION)
+            }
+        }
     }
 
     private fun setupUI() {
